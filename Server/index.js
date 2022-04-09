ffecb0ede7c34e52383c5d4eb7a5b1995e1a7716
Ernesto López <52114915+ernestolopezvv@users.noreply.github.com>
2022-04-08 19:24:58 -0600

    Conexion a base de datos Azure

--- Server/index.js
+++ Server/index.js
@@ -8,3 +8,34 @@ app.get("/", (req, res) => {
 app.listen(3001, () => {
     console.log('running on port 3001');
 });
+
+const { Connection, Request } = require("tedious");
+
+// Create connection to database
+const config = {
+  authentication: {
+    options: {
+      userName: "FinanchDB", // update me
+      password: "saludos-123" // update me
+    },
+    type: "default"
+  },
+  server: "itcorp.database.windows.net", // update me
+  options: {
+    database: "Financh", //update me
+    encrypt: true
+  }
+};
+
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("conexion!");
+  }
+});
+
+connection.connect();
