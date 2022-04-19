e3ed5c5bbf5879f60fb4b55639a8da8e5582f15f
Ernesto López <52114915+ernestolopezvv@users.noreply.github.com>
2022-04-19 12:26:23 -0600

    Revert "Localhost 5000"
    This reverts commit f26948d730f36e621f11952557f4d0ff4989c3fc.

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,41 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-
 const app = express()
-const port = 5000
-
-// Static Files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/img', express.static(__dirname + 'public/img'))
-app.use('/js', express.static(__dirname + 'public/js'))
 
 app.get("/", (req, res) => {
-  res.send("Saludos");
+    res.send("Saludos");
 })
 
-app.use(bodyParser.urlencoded({ extended : true }))
+app.listen(3001, () => {
+    console.log('running on port 3001');
+});
 
+const { Connection, Request } = require("tedious");
 
-// Listen on port 5000
-app.listen(port, () => console.log(`Listening on port ${port}`))
+// Create connection to database
+const config = {
+  authentication: {
+    options: {
+      userName: "FinanchDB", // update me
+      password: "saludos-123" // update me
+    },
+    type: "default"
+  },
+  server: "itcorp.database.windows.net", // update me
+  options: {
+    database: "Financh", //update me
+    encrypt: true
+  }
+};
+
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("conexion!");
+  }
+});
+
+connection.connect();
