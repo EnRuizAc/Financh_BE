17875b040e28562a22d9c367659148196c2e4e22
JALV01 <A01562304@itesm.mx>
2022-04-25 17:54:45 -0600

    Revisando conexion a 3001

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 const mysql = require('mysql')
-const cors = require('cors');
 
 app.get("/", (req, res) => {
     res.send("Saludos");
 })
 
 app.use(express.json());
-app.use(cors());
+
 
 const db = mysql.createConnection({
   user: "root",
@@ -16,6 +15,16 @@ const db = mysql.createConnection({
   password: "",
   database: "ITCORP",
 });
+
+app.listen(3001, () => {
+  db.connect(function(err){
+      if (err) throw err;
+      console.log("Connected!");
+    });
+  console.log("Funcionando en puerto 3001");
+});
+
+
 
 
 app.post("/registro", (req, res) => {
@@ -39,14 +48,46 @@ app.post("/registro", (req, res) => {
 });
 
 //----------------------------
+/*
+// Multer Upload Storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+  cb(null, __basedir + '/uploads/')
+  },
+  filename: (req, file, cb) => {
+  cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname)
+  }
+  });
 
-app.listen(3001, () => {
-  db.connect(function(err){
-      if (err) throw err;
-      console.log("Connected!");
-    });
-  console.log("Funcionando en puerto 3001");
+  const upload = multer({storage: storage});
+
+//! Routes start
+//route for Home page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
+// -> Express Upload RestAPIs
+app.post('/uploadfile', upload.single("uploadfile"), (req, res) =>{
+  importExcelData2MySQL(__basedir + '/uploads/' + req.file.filename);
+  console.log(res);
+});
+// -> Import Excel Data to MySQL database
+function importExcelData2MySQL(filePath){
+// File path.
+readXlsxFile(filePath).then((rows) => {
+// `rows` is an array of rows
+// each row being an array of cells.  
+  console.log(rows);
+
+/**
+[ [ 'nivel', 'codigo', 'nombre', 'tipo' ],
+[ 1, '000-0100', 'activo', 'A Activo Deudora' ],
+[ 2, '000-0110', 'circulante', 'A Activo Deudora' ],
+[ 3, '100-000', 'Fondo Fijo Caja', 'A Activo Deudora' ]
+*/
+
+//----------------------------
+
 
 
 app.get('/datos', (req, res) => {
