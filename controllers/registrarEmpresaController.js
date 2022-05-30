b9de75e5a8069d1eb66f2a5d5168f6e0124baffb
Andrea Karolina Andujo Vega <60046788+andykandujo@users.noreply.github.com>
2022-05-30 16:07:26 -0600

    Update registrarEmpresaController.js

--- controllers/registrarEmpresaController.js
+++ controllers/registrarEmpresaController.js
@@ -4,16 +4,16 @@ const config = require('../config/dbConn');
 
 const registrarEmpresa = async (req, res) => {
 
-    console.log("Testing Agregar Empresa");
-
-
+    const Nombre = req.body.Nombre;
+    const Sucursal = req.body.Sucursal;
+    if(!Nombre) return res.status(400).json({ 'message' : 'Name required.'});
 
     try {
+     
+        const pool = await sql.connect(config);
+        const nuevaEmpresa = await pool.request().query("INSERT INTO empresa (Nombre, Sucursal) VALUES ('" + Nombre + "',  '" + Sucursal +"')");
 
-        
-
-
-      
+        res.status(201).json({ 'success': `New empresa ${Nombre} created!` });
 
     } catch (error) {
        console.log(error);
@@ -21,7 +21,5 @@ const registrarEmpresa = async (req, res) => {
     }
 }
 
-
 module.exports = registrarEmpresa;
-    
-
+  
