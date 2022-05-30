6d80b7d2b90a63ada2d68c1e933916b1c6b67d17
ErnestoLV <ernestolopezvv@outlook.com>
2022-05-30 02:01:44 -0600

    allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,8 +3,9 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://localhost:3001'
-    
+    'http://localhost:3001',
+    "https://itcorp-front.herokuapp.com",
+    "http://itcorp-front.herokuapp.com"
 ];
 
 module.exports = allowedOrigins;
