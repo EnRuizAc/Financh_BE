5362f1ba2463deeb55b8dba7e011bb16e299fc52
Andrea Karolina Andujo Vega <60046788+andykandujo@users.noreply.github.com>
2022-05-30 16:44:27 -0600

    Update registrarEmpresa.js

--- routes/registrarEmpresa.js
+++ routes/registrarEmpresa.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const registrarEmpresa = require('../controllers/registrarEmpresaController');
+const getEmpresas = require('../controllers/verEmpresasController');
 
 router.post('/', registrarEmpresa);
+router.get('/', getEmpresas);
 
 module.exports = router;
